b9c0bf81a7ca5d39f288007b92a9ff805b5b9f79
sr447011 <rayswanagan@gmail.com>
2021-11-09 22:17:39 -0500

    added logo to main page

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,10 @@
 import React from 'react'
+import { StaticImage } from "gatsby-plugin-image"
 
 const Header = () => {
     return (
         <header className="pt-16 pb-10 text-center">
+        <StaticImage src="../images/logo.png" alt="ApolloFitnessLogo" />
         <h1 className="font-semibold lg:text-5xl sm:text-4xl text-2xl pb-4">Welcome to Apollo Fitness</h1>
         <p className=" lg:text-2xl sm:text-lg text-sm font-light">Your partner in your fitness journey</p>
         </header>
