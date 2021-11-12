357f92b7ad87d4c2bc11fc4dedbc58f86b18878a
sr447011 <rayswanagan@gmail.com>
2021-11-11 21:49:14 -0500

    added hamburger menu

--- src/components/layout.js
+++ src/components/layout.js
@@ -10,12 +10,15 @@ const Layout = ({children}) => {
     return (
         <div>
             <nav className="flex sm:justify-between justify-around items-center bg-gray-800 lg:px-20 sm:px-6 py-8">
-                <StaticImage src="../images/logo_w_words.png" alt="SwayFitLogoWords" />
+                <StaticImage src="../images/logo_w_words.png" alt="SwayFitLogoWords"/>
                 <ul className="hidden sm:flex flex-1 justify-end items-center gap-9 text-white font-notosans font-bold text-sm">
-                    <li className="Hover:opacity-70"><Link to="/">Home</Link></li>
-                    <li className="Hover:opacity-70"><Link to="/dashboard">Dashboard</Link></li>
-                    <button type="button" class="bg-purple-600 text-white rounded-md font-bold font-notosans px-7 py-3"><Link to="/login">Login</Link></button>
+                    <li className="Hover:opacity-70"><Link to="/">home</Link></li>
+                    <li className="Hover:opacity-70"><Link to="/dashboard">dashboard</Link></li>
+                    <button type="button" class="bg-purple-600 text-white rounded-md font-bold font-notosans px-7 py-3"><Link to="/login">login</Link></button>
                 </ul>
+                <div class="flex sm:hidden flex-1 justify-end px-6">
+                    <i class="text-2xl fas fa-bars text-white"></i>
+                </div>
             </nav>
 
             <main className="bg-gray-800">{children}</main>
