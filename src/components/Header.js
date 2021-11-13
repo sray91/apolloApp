82e2604652bac13b809f07c4255b231591818d97
sr447011 <rayswanagan@gmail.com>
2021-11-12 23:21:12 -0500

    fixed video issues

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,20 @@
 import React from 'react'
-import Video from '../videos/heroVideo.mp4'
+import Video from '../videos/heroVideo3.mp4'
 
 // this is the "header" for the main page
 
 const Header = () => {
     return (
-        <header className="text-center position: absolute inset-0 overflow-hidden w-full h-full">
-            <video autoPlay loop className="w-full h-full object-cover">
+        <section className="position: relative flex items-center justify-center h-screen sm:h-96 -my-30 py-36 overflow-hidden bg-gray-800">
+            <video autoPlay loop muted playsInline className="position: absolute z-10 w-auto min-w-full min-h-full max-w-none">
                 <source src={Video} type="video/mp4" />
                 <track kind="captions" srcLang="en" src="" />
             </video>
-            <h1 className="font-notosans font-bold lg:text-5xl sm:text-4xl text-2xl pb-4 text-white z-20 max-h-full">Welcome to SwayFit!</h1>
+            <div className="flex flex-col position: relative z-30 text-center">
+                <h1 className="font-notosans font-bold lg:text-5xl sm:text-4xl text-2xl pb-4 text-white">Welcome to SwayFit!</h1>
                 <p className="font-notosans lg:text-2xl sm:text-lg text-sm text-white">Your fitness journey partner.</p>
-        </header>
+            </div>
+        </section>
     )
 }
 
