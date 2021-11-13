1c22e194a3d5e94fb1b8a869de462f377462273f
sr447011 <rayswanagan@gmail.com>
2021-11-12 22:14:16 -0500

    added video

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,16 @@
 import React from 'react'
+import Video from '../videos/heroVideo.mp4'
 
 // this is the "header" for the main page
 
-const Header = () => {    
+const Header = () => {
     return (
-        <header className="pt-16 pb-10 text-center bg-gray-800">
-            <h1 className="font-notosans font-bold lg:text-5xl sm:text-4xl text-2xl pb-4 text-white">Welcome to SwayFit!</h1>
+        <header className="text-center position: absolute inset-0 overflow-hidden w-full h-full">
+            <video autoPlay loop className="w-full h-full object-cover">
+                <source src={Video} type="video/mp4" />
+                <track kind="captions" srcLang="en" src="" />
+            </video>
+            <h1 className="font-notosans font-bold lg:text-5xl sm:text-4xl text-2xl pb-4 text-white z-20 max-h-full">Welcome to SwayFit!</h1>
                 <p className="font-notosans lg:text-2xl sm:text-lg text-sm text-white">Your fitness journey partner.</p>
         </header>
     )
