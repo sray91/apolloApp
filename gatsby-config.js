562c67d3420b84e93f98eb9d3ff14ff07ca23af1
sr447011 <rayswanagan@gmail.com>
2021-11-17 22:34:40 -0500

    latest changes

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,7 +5,7 @@ module.exports = {
     description: "Fitness app for android, iOS, and web. Web3 integration. Machine learning and AI. Fitness partner",
   },
   plugins: [
-    'gatsby-plugin-postcss',
+    `gatsby-plugin-postcss`,
     `gatsby-plugin-image`,
     `gatsby-plugin-sharp`,
     `gatsby-transformer-sharp`,
@@ -13,7 +13,30 @@ module.exports = {
     {
       resolve: `gatsby-source-filesystem`,
       options: {
-        path: `./src/data/`,
+        name: `images`,
+        path: `${__dirname}/src/images/`,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-sharp`,
+      options: {
+        // Defaults used for gatsbyImageData and StaticImage
+        defaults: {},
+        // Set to false to allow builds to continue on image errors
+        failOnError: false,
+        // deprecated options and their defaults:
+        base64Width: 20,
+        forceBase64Format: `png`, // valid formats: png,jpg,webp
+        useMozJpeg: process.env.GATSBY_JPEG_ENCODER === `MOZJPEG`,
+        stripMetadata: true,
+        defaultQuality: 50,
+      },
+    },
+    {
+      resolve: `gatsby-transformer-sharp`,
+      options: {
+        // The option defaults to true
+        checkSupportedExtensions: false,
       },
     },
   ],
--- src/components/About.js
+++ src/components/About.js
@@ -1,11 +1,13 @@
 import * as React from 'react'
+//import { useStaticQuery, graphql } from 'gatsby'
+//import { GatsbyImage, getImage } from "gatsby-plugin-image"
 
 const About = () => {
     return (
         <section className="position: relative min-h-screen py-20 bg-gray-800">
             <div>
                 <h1 className="text-2xl text-center mb-20 text-white">Heading</h1>
-                <p>Wrapper</p>
+                <p>wrapper</p>
             </div>
         </section>
     )
